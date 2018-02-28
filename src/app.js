16fc92c86dec9bc246773eb601d640f542a179f0
user3520 <gospod.ivanovich@yandex.ru>
2018-02-28 16:18:26 +0300

    first lesson

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import List from "./module2List";
+import {articles} from "./fixtures";
+/*
 class App extends React.Component {
   render() {
     return <div>The App</div>;
   }
+//  return <div>The App</div>;
 }
 
+function Hallo() {
+  return <div>Hallo</div>
+} 
+*/
 const Mount_Node = document.getElementById("app");
-
-ReactDOM.render(
-  <App />,
-  Mount_Node
-);
+// ReactDOM.render(<Hallo />, document.getElementById("app"))
+// ReactDOM.render(<App />, Mount_Node);
+ReactDOM.render(<List blabla = {articles} />, Mount_Node);
