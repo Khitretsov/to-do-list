6b2237bd2271ca50b9f523c6beb3ff02f47eab0c
user3520 <gospod.ivanovich@yandex.ru>
2018-03-02 02:43:41 +0300

    ещё остались ошибки при удалении строк

--- src/createStr.js
+++ src/createStr.js
@@ -10,18 +10,11 @@ class CreateStr extends Component {
     render () {
         let  i = this.props.ind
         return (
-            <ul>{this.props.str} <button onClick={this.remove(i)}>Убрать от сюда</button></ul>
+            <ul> {this.props.str} <button onClick={this.props.delete}>Убрать от сюда</button></ul>
         )
-    }
 
-    // i = this.props.ind
-    remove(i) {
-        let copy = this.props.str
-        // .slice();
-      //  copy.splice(i, 1)
-      //  this.setState({ str: copy })
-      console.log(copy.splice(i, 1))
-      console.log('-------', typeof(this.props.ind))
+     //  <button onClick={this.remove(i)}>
+     // 
     }
 }
 
--- src/module.js
+++ src/module.js
@@ -5,39 +5,42 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-          //  count: 0
             str: [],
             current: ''           
         }
-        this.incr = this.incr.bind(this)
-       // this.onInput = this.onInput.bind(this)
-        this.onClick = this.onClick.bind(this)
-    //    this.remove = this.remove.bind(this)
-    }
-
-    incr() {
-        this.setState({ count: this.state.count + 1 });
     }
 
     onInput = e => {
         this.setState({ current: e.target.value })
     }
 
-    onClick() {
+    onClick = () => {
         let copy = this.state.str.slice();
         copy.push(this.state.current); 
 
         this.setState({ str: copy, current: ""});
     }
 
+    deleteStr = i => {
+        let copy = this.state.str.slice();
+        copy.splice(i, 1);
 
+        this.setState({ str: copy });
+    }
 
     render() {
-        let list = this.state.str.map(function(e, i) {
+ /*    let list =  this.state.str.map((e, i) => {
             return (
-                <CreateStr str={e} ind={i}/>
+                <li key = {i}> {e} <button>Это кнопка</button> </li>  // Почему-то не говорит про уникальный ключ
+            )
+        })*/
+        let list = this.state.str.map((e, i) => {
+            return (
+                <CreateStr str={e} delete={() => this.deleteStr(1)}/>
             )
         })
+
+
         return (
             <div>
                 <input placeholder="Писать сюда" value={this.state.current} onChange={this.onInput}/>
@@ -46,12 +49,6 @@ class App extends Component {
                 {this.state.str.length === 0 ? "Пусто" : <ul>{list}</ul>}
             </div>
         )
-    /*    return (
-            <div>
-                <button onClick={this.incr} >Add</button>
-                {this.state.count}
-            </div>
-        ) */
     }
 }
 
