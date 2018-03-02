3cb8812490175b4a9559449c0de805b652ee05cf
user3520 <gospod.ivanovich@yandex.ru>
2018-03-02 17:44:00 +0300

    Кнопка итак могла работать, просто я глупые ошибки

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,16 @@
 import React from "react";
 
-/*
+
 export default function Button(props) {
     return(
-        <button onClick={props.onSubmit}>Это кнопка</button> 
+        <button onClick={props.onClick}>Это кнопка</button> 
     );
 }
-*/
 
+/*
 export default function Button(props) {
   const onClick = props.onClick;
   const children = props.children;
   return <button onClick={onClick}>{children}</button>;
 }
+*/
