6d34a5c441a77a42d9397bc11313ce45a72075f8
user3520 <gospod.ivanovich@yandex.ru>
2018-03-02 14:41:27 +0300

    я нихера непонимаю

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -4,6 +4,6 @@ export default function InputBox(props) {
     const onChange = props.onChange;
     const value = props.value;
     return(
-        <input value={value} onChange={e => onChange(e.target.value)} />
+        <input value={value} onChange={onChange()} />
     );
 }
